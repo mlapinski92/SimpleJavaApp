20a6423b789bc7c29ff820d73b86313778bb07c0
mlapinski92 <maciej.lapinski92@gmail.com>
2019-12-06 11:44:54 +0700

    JustChecking

--- src/SimpleJavaApp/MainApp.java
+++ src/SimpleJavaApp/MainApp.java
@@ -1,4 +1,32 @@
 package SimpleJavaApp;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
 public class MainApp {
+    private JButton Login;
+    private JPanel panelMain;
+    private JPasswordField passwordField1;
+    private JTextField textField1;
+    private JButton loginButton;
+    private JButton exitButton;
+
+
+    public MainApp() {
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+    public static void main(String[] args) {
+
+    }
+
+
+
 }
